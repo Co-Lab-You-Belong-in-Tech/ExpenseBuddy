12e752459f38096223c2485af3d05a80887e2f3a
Gian Carlo Torres <geanu@outlook.com>
2023-10-12 23:24:10 -0400

    Working Models, api/Routes and Database Migration

--- BackEnd/models.py
+++ BackEnd/models.py
@@ -18,7 +18,7 @@ def load_user(user_id):
     return User.query.get(user_id)
 
 class User(db.Model, UserMixin):
-    id = db.Column(db.String, primary_key=True)
+    user_id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(150), nullable=True, default='')
     last_name = db.Column(db.String(150), nullable = True, default = '')
     email = db.Column(db.String(150), nullable = False)
@@ -28,7 +28,7 @@ class User(db.Model, UserMixin):
     date_created = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
 
     def __init__(self, email, first_name='', last_name='', password='', token='', g_auth_verify=False):
-        self.id = self.set_id()
+        self.user_id = self.set_user_id()
         self.first_name = first_name
         self.last_name = last_name
         self.password = self.set_password(password)
@@ -39,7 +39,7 @@ class User(db.Model, UserMixin):
     def set_token(self, length):
         return secrets.token_hex(length)
 
-    def set_id(self):
+    def set_user_id(self):
         return str(uuid.uuid4())
     
     def set_password(self, password):
@@ -50,12 +50,13 @@ class User(db.Model, UserMixin):
         return f'User {self.email} has been added to the database'
 
 class Expense(db.Model):
-    expense_id = db.Column(db.String, primary_key = True)
+    expense_id = db.Column(db.Integer, primary_key = True)
     expense_date = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
     expense_type = db.Column(db.String(150), nullable = False)
     expense_dollar_amt = db.Column(db.NUMERIC(10,2))
     expense_mileage = db.Column(db.NUMERIC(10,2))
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable = False)
     user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
 
     def __init__(self, expense_date, expense_type, expense_dollar_amt, expense_mileage, expense_id = ''):
@@ -64,7 +65,6 @@ class Expense(db.Model):
         self.expense_type = expense_type
         self.expense_dollar_amt = expense_dollar_amt
         self.expense_mileage = expense_mileage
-
 
     def __repr__(self):
         return f'The following bottle has been added to your home bar: {self.brand}'
@@ -80,24 +80,25 @@ expense_schema = ExpenseSchema()
 expenses_schema = ExpenseSchema(many=True)
 
 class Address(db.Model):
-    address_id = db.Column(db.String, primary_key = True)
+    address_id = db.Column(db.Integer, primary_key = True)
     street_1 = db.Column(db.String(150), nullable = False)
     street_2 = db.Column(db.String(150))
     city = db.Column(db.String(150))
-    state = db.Column(db.String(2))
-    zip = db.Column(db.NUMERIC(5))
-    address_type = db.Column(db.String(150))
+    statecode = db.Column(db.String(2))
+    zipcode = db.Column(db.NUMERIC(5))
+    address_name = db.Column(db.String(150))
 
+    user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable = False)
     user_token = db.Column(db.String, db.ForeignKey('user.token'), nullable = False)
 
-    def __init__(self, street_1, street_2, city, state, zip, address_type, address_id = ''):
+    def __init__(self, street_1, street_2, city, statecode, zipcode, address_name, address_id = ''):
         self.address_id = self.set_id()
         self.street_1 = street_1
         self.street_2 = street_2
         self.city = city
-        self.state = state
-        self.zip = zip
-        self.address_type = address_type
+        self.statecode = statecode
+        self.zipcode = zipcode
+        self.address_type = address_name
 
     def __repr__(self):
         return f'The address has been added!'
@@ -107,7 +108,7 @@ class Address(db.Model):
     
 class AddressSchema(ma.Schema):
     class Meta:
-        fields = ['address_id','street_1', 'street_2', 'city' ,'state', 'zip', 'address_type']
+        fields = ['address_id','street_1', 'street_2', 'city' ,'statecode', 'zipcode', 'address_type']
 
 address_schema = AddressSchema()
 addresses_schema = AddressSchema(many=True)
