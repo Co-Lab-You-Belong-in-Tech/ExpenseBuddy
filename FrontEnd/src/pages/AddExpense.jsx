568edbb4feab2c41f25a165524239362b9f03222
Gian Carlo Torres <geanu@outlook.com>
2023-10-23 23:53:59 -0400

    Resolved the navbar style issues after assigning the routes.

--- FrontEnd/src/pages/AddExpense.jsx
+++ FrontEnd/src/pages/AddExpense.jsx
@@ -1,7 +1,18 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: auto;
+    height: 75vh;
+    align-items: center;
+    justify-content: center;
+    background-color: azure;
+    padding: 16px 20px;
+`
+
 export default function AddExpense() {
   return (
-    <>
+    <Container>
         <h2>Add Expense Page</h2>
-    </>
+    </Container>
   )
 }
--- FrontEnd/src/pages/Settings.jsx
+++ FrontEnd/src/pages/Settings.jsx
@@ -1,8 +1,19 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+    width: auto;
+    height: 75vh;
+    align-items: center;
+    justify-content: center;
+    background-color: azure;
+    padding: 16px 20px;
+`
+
 export default function Settings() {
   return (
-    <>
+    <Container>
         <h2>Settings Page</h2>
-    </>
+    </Container>
   )
 }
 
